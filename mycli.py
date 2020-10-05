2c83913c2bdc9378fe079e57e2b9ecb428bdc326
odergatter <odergatter@mailbox.org>
2020-10-05 15:39:55 +0200

    Usage added

--- mycli.py
+++ mycli.py
@@ -7,7 +7,8 @@ def main():
             print(datetime.datetime.now())
         elif sys.argv[1] == "utcnow":
             print(datetime.datetime.utcnow())
-
+    else:
+        print(f'Usage: python3 {__file__} (now | utcnow)')
 
 if __name__ == "__main__":
     main()
